f7b034f9b7e703f72e4cc63b8b0151e81916dfa7
WINDY-WINDWARD <karthik.sarode23@gmail.com>
2022-12-11 15:12:09 +0530

    QR builder completed

--- Routes/qrGen.py
+++ Routes/qrGen.py
@@ -1,12 +1,19 @@
 import pyqrcode
 from pyqrcode import QRCode
 import os
+from dotenv import load_dotenv
+import io
 
+load_dotenv()
 def generateQR(s):
     # get web_page from .env 
     a = os.environ.get('WEB_PAGE')
-    a = a+"/getCustomer/login?key="+s
+    print(a)
+    a = a+"getCustomer/signUp?key="+s
     # Generate QR code
-    url = pyqrcode.create(s)
-    # Create and save the svg file naming "myqr.svg"
-    return url.svg("myqr.svg", scale = 8)
+    url = pyqrcode.create(a)
+    buffer = io.BytesIO()
+    url.svg(buffer, scale=8)
+    return buffer.getvalue()
+
+# generateQR("1234567890")
