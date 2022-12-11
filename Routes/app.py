f77980defc8a991c5bc1bafef0e8008fba7f5560
BabaChiku <ashishedu60@gmail.com>
2022-12-11 14:18:33 +0530

    About url_for

--- Routes/app.py
+++ Routes/app.py
@@ -113,5 +113,9 @@ def signUp():
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(threaded=True, port=5000, debug=True)
